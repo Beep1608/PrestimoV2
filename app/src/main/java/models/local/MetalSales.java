0cf880356672745c87294dd62b5a13ec76a9af29
osvi-dev <joseconstantino987@gmail.com>
2025-01-30 23:19:25 -0600

    Feature: db, and view of metal sales

---
+++ app/src/main/java/models/local/MetalSales.java
@@ -0,0 +1,4 @@
+package models.local;
+
+public class MetalSales {
+}
---
+++ app/src/main/java/views/MetalSalesView.java
@@ -0,0 +1,10 @@
+package views;
+
+import javafx.scene.layout.Region;
+import javafx.util.Builder;
+
+import java.io.Serializable;
+
+public class metalSalesView implements Builder<Region> {
+
+}
